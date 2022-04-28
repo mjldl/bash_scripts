63f5c8d6c004d06077f05f36bc6531e101fdbb3d
Matheus Jabes <matheus.jabesldl@gmail.com>
2022-04-27 23:52:18 -0300

    terceira questão!

--- lista2_q3.sh
+++ lista2_q3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 t=$(wc -l < $1)
-ls -l > $1 &> /dev/null && (($t >= 3)) &> /dev/null && echo "GOOD" || echo "NOT GOOD"
+cat $1 &> /dev/null && (($t >= 3)) &> /dev/null && echo "GOOD" || echo "NOT GOOD"
